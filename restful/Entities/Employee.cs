1a399af97d6b1bd2486b26645313ac477891b290
hitmachut <hitmachut@students.local>
2023-11-09 11:06:50 +0200

    hotel

---
+++ restful/Entities/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace restFul.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public bool Status { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ restful/Entities/Guest.cs
@@ -0,0 +1,10 @@
+﻿namespace restFul.Entities
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public int Phone { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
